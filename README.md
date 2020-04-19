3b10619affca9a9eec1f35cb9053bbbfe926d8f5
Nick <miller.nick.c@gmail.com>
2020-04-19 10:34:30 -0700

    Update 2

---
+++ README.md
@@ -0,0 +1,3 @@
+Changed all the background color for the sidebar to a dark blue #393782
+
+Also changed the default backgrounds in the foundation file to #393782
--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -9,31 +9,30 @@ app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
-	res.render('index', { data: data })
+	return res.render('index', { data: data }); 
 	})
 
 app.get('/about', (req, res) => {
-	res.render('about')
+	return res.render('about');
 	})
 
 app.get('/projects/:id', (req, res) => {
-	console.log(data[req.params.id])
-	res.render('project', { data: data[req.params.id] })
+	return res.render('project', { data: data[req.params.id] });
 	})
 
-//an example of the error status being set to 404 in function creating the error
-// app.use((req,res, next) => {
-// 	const err = new Error('File Not Found');
-// 	err.status = 404;
-// 	next(err);
-// })
+//404 error being created if no route is found
+app.use((req,res, next) => {
+	const err = new Error('File Not Found');
+	err.status = 404;
+	next(err);
+})
 
 
-// app.use((err, req, res, next) => {
-// 	console.log(err)
-// 	res.status(err.status);
-// 	res.locals.error = err;
-// 	res.render('error');
-// })
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.status(err.status);
+	res.locals.error = err;
+	res.render('error');
+})
 
-app.listen(3000)
+app.listen(3000, console.log("App now running on Port 3000"))
